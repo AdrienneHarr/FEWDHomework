bcfe810424703932be08a59128f6f8db5986da0f
AdrienneHarr <aharris92.ah@gmail.com>
2023-02-09 20:54:00 -0500

    lesson 8

--- Lesson 08/01_compare_that/starter_code/js/index.js
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -6,6 +6,33 @@
      let b = Number(document.querySelector('#b').value);
  
      // Add comparison logic
+
+     // if a is greater than b
+
+     if (a > b){
+      comparison = " >";
+     }
+
+     // if b is gerater than a
+
+     if (b > a){
+      comparison = "<"
+     }
+
+     // if they are equal
+
+     if ( a == b ){
+      comparison = "=="
+     }
+
+     // if a OR b isn't a number
+
+     if (isNaN(a) || isNaN(b)){
+      comparison = "N/A";
+     }
+     
+     console.log (a);
+     console.log (b);
  
      document.querySelector('#comparison').innerText = comparison;
    };
--- Lesson 08/04_color_switcher/starter_code/script.js
+++ Lesson 08/04_color_switcher/starter_code/script.js
@@ -1,4 +1,23 @@
-// When the #grayButton is clicked, the color scheme of the page should change to more of a "dark mode" feel, with gray backgrounds and white text throughout.
+// When the #grayButton is clicked, 
+document.getElementById('grayButton').addEventListener('click',function(){
+    //the color scheme of the page should change to more of a "dark mode" feel, with gray backgrounds and white text throughout.
+    document.body.style.backgroundColor = "#555555";
+    document.body.style.color = "#FFFFFF";
+});
+
+// the color scheme of the page should change to more of a "dark mode" feel, with gray backgrounds and white text throughout.
+
+
+
 // The #whiteButton should bring things back to the "light mode" color scheme with white backgrounds and dark text
+
+document.getElementById('whiteButton').addEventListener('click',function(){
+    //the color scheme of the page should change to more of a "dark mode" feel, with gray backgrounds and white text throughout.
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+});
+
 // Try to do this by setting styles (.style)
+
+
 // Remember to try one step at a time, testing each stage as you go!
